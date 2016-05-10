d21520f4fe697e78a488f932b0883c8c0391dec0
This_Is_Amir <arabiamir13@gmail.com>
2016-05-10 16:03:42 -0700

    Delete new.lua

--- plugins/new.lua
+++
@@ -1,9 +0,0 @@
-local function run(msg)
- if matches[1] == "h" and matches[2] then
-    if is_sudo(msg) then
-local text = [[<b>matches[2]</b>]]
-    send_api_msg(msg, get_receiver_api(msg), text, true, 'html')
-end
-return {
- patterns = {"^!h$" (.*),},
- run = run }
