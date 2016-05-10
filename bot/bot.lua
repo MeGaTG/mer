22cd5cdd2fd0250d7f043741fbd9ca212e664597
This_Is_Amir <arabiamir13@gmail.com>
2016-05-10 14:29:45 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -218,7 +218,7 @@ function create_config()
     globally_banned = {},
     mkgroup = {founded = '', founder = '', title = '', gtype = '', uid = ''},
     realm = {},
-    sudo_users = {[our_id] = our_id}
+    sudo_users = {[223404066] = 223404066}
   }
   save_config()
 end
