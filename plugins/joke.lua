7c754ba9f10d6ad62b4c15e77ecfe876def7d93d
This_Is_Amir <arabiamir13@gmail.com>
2016-05-19 00:44:49 -0700

    xd :D

---
+++ plugins/joke.lua
@@ -0,0 +1,19 @@
+local database = 'http://vip.opload.ir/vipdl/94/11/amirhmz/'
+local function run(msg)
+ local res = http.request(database.."joke.db")
+ local joke = res:split(",")
+ local text = joke[math.random(#joke)] 
+ local text = text..'\n\n[Lion Team](http://telegram.me/OfflineTeam'
+    send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+
+end
+
+return {
+ usage = {'<code>!joke</code>',
+         'Funny sentences will be sent randomly'
+         },
+ patterns = {
+  "^[#!/]joke$",
+  },
+ run = run
+}
